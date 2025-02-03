ed907cdcb218b40a384c699ebd983727a1c712de
mtnleo <mtnleonardi@gmail.com>
2025-02-02 19:21:46 -0700

    ECMAScript modules intro

---
+++ FirstSteps/index.mjs
@@ -0,0 +1,7 @@
+// CommonJS import
+//const sum = require('./sum.mjs');
+import { sum, sub, mult } from './sum.mjs';
+
+console.log(sum(4, 9));
+console.log(sub(3, 6));
+console.log(mult(4, 9));
---
+++ FirstSteps/sum.mjs
@@ -0,0 +1,14 @@
+export function sum(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export function mult(a, b) {
+    return a * b;
+}
+
+// CommonJS export
+// module.exports = sum;
